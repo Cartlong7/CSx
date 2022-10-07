9f6831887e354deb3788436c208b79ec0756ef2c
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-07 11:20:22 -0600

    Create delay.js
    Write a function delay that accepts two arguments, a callback and the wait time in milliseconds. Delay should return a function that, when invoked waits for the specified amount of time before executing. HINT - research setTimeout();

---
+++ delay.js
@@ -0,0 +1,13 @@
+const delay = (cb, wait) => {
+  const waitTime = () => {
+    setTimeout(cb, wait);
+  }
+  return waitTime;
+}
+
+// UNCOMMENT THE CODE BELOW TO TEST DELAY
+let count = 0;
+const delayedFunc = delay(() => count++, 1000);
+delayedFunc();
+console.log(count); 												 // should print '0'
+setTimeout(() => console.log(count), 1000); // should print '1' after 1 second
