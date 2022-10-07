caf6a8c810732bb5fae6c0335189c7ca1ec3672a
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-07 11:18:40 -0600

    Create scoping.js
    Examine the code for the outer function. Notice that we are returning a function and that function is using variables that are outside of its scope. Uncomment those lines of code. Try to deduce the output before executing.

---
+++ scoping.js
@@ -0,0 +1,21 @@
+function outer() {
+  let counter = 0; 
+  function incrementCounter() {
+    counter++;
+    console.log('counter', counter);
+  }
+  return incrementCounter;
+}
+
+const willCounter = outer();
+const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+// willCounter();
+// willCounter();
+// willCounter();
+
+// jasCounter();
+// willCounter();
