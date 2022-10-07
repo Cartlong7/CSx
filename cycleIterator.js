f4a0becface204ea36e9612260e752ab87937681
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-07 11:21:15 -0600

    Create cycleIterator.js
    Create a function cycleIterator that accepts an array, and returns a function. The returned function will accept zero arguments. When first invoked, the returned function will return the first element of the array. When invoked a second time, the returned function will return the second element of the array, and so forth. After returning the last element of the array, the next invocation will return the first element of the array again, and continue on with the second after that, and so forth.

---
+++ cycleIterator.js
@@ -0,0 +1,20 @@
+const cycleIterator = (array) => {
+	let index = 0;
+  const whatDayIsIt = () => {
+    const result = array[index++];
+    if (index >= array.length) index = 0;
+    return result
+  };
+  return whatDayIsIt
+}
+
+
+
+// Uncomment these to check your work!
+const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+const daysOfWeek = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+const getDay = cycleIterator(daysOfWeek);
+console.log(getDay()); // should log: 'Fri'
+// console.log(getDay()); // should log: 'Sat'
+// console.log(getDay()); // should log: 'Sun'
+// console.log(getDay()); // should log: 'Fri'
