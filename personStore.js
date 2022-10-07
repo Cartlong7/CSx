b82b6b9a2683556de4e92f1bd2921142873f5f69
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-07 08:52:34 -0600

    Create personStore.js
    Inside personStore object, create a property greet where the value is a function that logs "hello".

---
+++ personStore.js
@@ -0,0 +1,9 @@
+const personStore = {
+	// create a property greet where the value is a function that logs hello
+  greet : function () {
+    console.log('hello');
+  }
+};
+
+// Uncomment this line to check your work!
+personStore.greet(); // -> Logs 'hello'
