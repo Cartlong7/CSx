05131422b056b27db3f6f5090a18635ce34906e7
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-09 20:00:49 -0600

    Create shuffleCards.js
    You are creating a card game application with your friend.
    
    She already wrote a function that divides the deck of cards into top and bottom halves, but needs help writing a function that shuffles the two halves together again.
    Challenge
    
    Write a function that takes two arrays as inputs, representing the top and bottom halves of a deck of cards, and shuffles them together. The function will return a single array containing the elements from both input arrays interleaved, like so:
    
    the first element should be the first element of the first input array,
    
    the second element should be the first element of the second input array,
    
    the third element should be the second element of the first input array,
    
    the fourth element should be the second element of the second array,
    
    and so on.
    
    The arrays may be of different lengths. After interleaving the elements of the input arrays, any remaining elements should be appended to the end of the array.
    
    This problem can be solved in many ways, but try to solve it with recursion!

---
+++ shuffleCards.js
@@ -0,0 +1,13 @@
+const shuffleCards = ([value, ...topHalf], bottomHalf) => value === undefined ? [...bottomHalf] : [value, ...shuffleCards(bottomHalf, topHalf)];
+// UNCOMMENT TO TEST YOUR WORK
+// const topHalf = ['Queen of Diamonds', 'Five of Hearts', 'Ace of Spades', 'Eight of Clubs'];
+// const bottomHalf = ['Jack of Hearts', 'Ten of Spades'];
+// console.log(shuffleCards(topHalf, bottomHalf));
+//   /*-> ['Queen of Diamonds',
+//         'Jack of Hearts',
+//         'Five of Hearts',
+//         'Ten of Spades',
+//         'Ace of Spades',
+//         'Eight of Clubs',
+//       ]
+//   */
