63b0914b6d121bcb38004c0fda8111e429a6d9cf
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-14 14:52:46 -0600

    Update createFunction.js
    Create a function createFunction that creates and returns a function. When that newly created function is called, it should return the string 'hello world'.

--- createFunction.js
+++ createFunction.js
@@ -1,12 +1,7 @@
-// declare a function createFunction with no parameters
 const createFunction = () => {
-	// the function returns a function, and when called returns the string 'hello world'
-  const sayHello = () => {
- return 'hello world'; 
-  }
-  return sayHello;
+  const hello = () => 'hello world';
+  return hello;
 }
-
 
 // Uncomment these to check your work!
 const myFunction = createFunction();
