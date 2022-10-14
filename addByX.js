386be2d518789b39b0115b9e791b25d57dbc4aeb
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-14 15:01:29 -0600

    Update addByX.js
    Now we are going to create a function addByX that returns a function that will add an input by x.

--- addByX.js
+++ addByX.js
@@ -1,12 +1,9 @@
-const addByX = (num) => {
-  const addInput = (x) => {
-    return num += x;
-  }
-  return addInput;
+const addByX = (input) => {
+  const addNum = (num) => num += input;
+  return addNum;
 }
 
 const addByTwo = addByX(2);
-
 // Now call addByTwo with an input of 1 and log the output
 console.log(addByTwo(1));
 // Now call addByTwo with an input of 2 and log the output
