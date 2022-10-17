432fa537864006a7c3329b91cda29c08015243c2
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-10-17 17:01:19 -0600

    Update after.js
    Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter.

--- after.js
+++ after.js
@@ -1,14 +1,17 @@
 // declare a function that takes two parameters (# of times cb needs to be called before executed, and the cb)
-const after = (callCount, cb) => {
-  let counter = 0; // initialize a variable that is set to 0 that will be incremented
+  // initialize a variable that is set to 0 that will be incremented
   // create a function that uses the rest parameter to store elements
-  const numTimesRun = (...args) => {
-    counter++; // increment counter
-    if (counter >= callCount) {  // if counter is greater than or equal to callCount
-      return cb(...args); // return cb with the argument of spread parameter args
-    }
+    // increment counter
+    // if counter is greater than or equal to callCount
+      // return cb with the argument of spread parameter args
+const after = (num, cb) => {
+  let counter = 0;
+  return function(...args) {
+    counter++;
+    if (counter >= num) {
+			return cb(...args);
+    } 
   }
-  return numTimesRun;
 }
 
 const called = function(string) { return('hello ' + string); };
