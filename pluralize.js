e5f0bc796396bb9fd087a8fbe38d2a75a7cb29cd
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-11-02 10:23:39 -0600

    Update pluralize.js
    Create a function pluralize that takes an array of strings as input and returns a new array with an "s" added to the end of each string in the input array. For example, if the string "carrot" is in the input array, it should become the string "carrots" in the output array.
    
    The body of the pluralize function should employ a single for loop that pluralizes each string in the input array.

--- pluralize.js
+++ pluralize.js
@@ -1,7 +1,17 @@
-const pluralize = (stringArr) => {
+// implementing the forEach method
+const pluralize = stringArr => {
   const plurArr = [];
-	stringArr.forEach(el => plurArr.push(el + 's'));
+  stringArr.forEach(el => plurArr.push(el + 's'));
   return plurArr;
+}
+
+// Using a for loop
+const pluralize = arrOfStrings => {
+  const pluralArr = [];
+  for (let i = 0; i < arrOfStrings.length; i++) {
+    pluralArr.push(arrOfStrings[i] + 's');
+  }
+  return pluralArr;
 }
 
 // Uncomment these to check your work!
