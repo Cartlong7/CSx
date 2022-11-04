46722eccb2a0f8f96e9c6b97373ad72caf18e766
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-11-04 14:45:21 -0600

    Update getLength.js
    Get the length of an array using recursion without accessing its length property.

--- getLength.js
+++ getLength.js
@@ -1,4 +1,12 @@
 const getLength = (array, index = 0) => array[index] === undefined ? index : getLength(array, ++index);
+
+// OR
+
+function getLength(array, index = 0) {
+	if (array[index] === undefined) return index;
+  return getLength(array, ++index);
+}
+
 // To check if you've completed the challenge, uncomment these console.logs!
 console.log(getLength([1])); // -> 1
 console.log(getLength([1, 2])); // -> 2
