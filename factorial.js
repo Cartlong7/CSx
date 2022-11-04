2fe556ede8ba59f271922e94aa24c048251a8887
Cartlong7 <107726720+Cartlong7@users.noreply.github.com>
2022-11-04 14:33:37 -0600

    Update factorial.js
    Write a function that returns the factorial of a number.
    
    EXAMPLE4! = 4 * 3 * 2 * 1 = 24, so calling factorial(4) should return 24.

--- factorial.js
+++ factorial.js
@@ -1,5 +1,11 @@
-	// base case: when counter is less than or equal to 1, return 1
-  // recursive case: num * the recursive call of num - 1
+// Intuitive, more readable solution
+const factorial = num => {
+	if (num <= 1) return num;
+	return num * factorial(num - 1);
+}
+
+// base case: when counter is less than or equal to 1, return 1
+// recursive case: num * the recursive call of num - 1
 const factorial = (num, product = 1) => num === 1 ? product: factorial(num - 1, product * num)
 
 // To check if you've completed the challenge, uncomment these console.logs!
